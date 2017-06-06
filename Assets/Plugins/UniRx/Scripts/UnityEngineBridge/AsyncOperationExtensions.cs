482f4710c0227c9ea15f3b154a493cf568182f58
Emmanuel Jacquier <emmanuel.jacquier@gmail.com>
2017-06-06 15:48:51 -0400

    Fixed Rx namespace

--- Assets/Plugins/UniRx/Scripts/UnityEngineBridge/AsyncOperationExtensions.cs
+++ Assets/Plugins/UniRx/Scripts/UnityEngineBridge/AsyncOperationExtensions.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Threading;
 using UnityEngine;
+using Rx = UniRx;
 
 #if !UniRxLibrary
 using ObservableUnity = UniRx.Observable;
@@ -14,19 +15,19 @@ namespace UniRx
         /// <summary>
         /// If you needs return value, use AsAsyncOperationObservable instead.
         /// </summary>
-        public static IObservable<AsyncOperation> AsObservable(this AsyncOperation asyncOperation, IProgress<float> progress = null)
+        public static Rx.IObservable<AsyncOperation> AsObservable(this AsyncOperation asyncOperation, IProgress<float> progress = null)
         {
             return ObservableUnity.FromCoroutine<AsyncOperation>((observer, cancellation) => AsObservableCore(asyncOperation, observer, progress, cancellation));
         }
 
         // T: where T : AsyncOperation is ambigious with IObservable<T>.AsObservable
-        public static IObservable<T> AsAsyncOperationObservable<T>(this T asyncOperation, IProgress<float> progress = null)
+        public static Rx.IObservable<T> AsAsyncOperationObservable<T>(this T asyncOperation, IProgress<float> progress = null)
             where T : AsyncOperation
         {
             return ObservableUnity.FromCoroutine<T>((observer, cancellation) => AsObservableCore(asyncOperation, observer, progress, cancellation));
         }
 
-        static IEnumerator AsObservableCore<T>(T asyncOperation, IObserver<T> observer, IProgress<float> reportProgress, CancellationToken cancel)
+        static IEnumerator AsObservableCore<T>(T asyncOperation, Rx.IObserver<T> observer, IProgress<float> reportProgress, CancellationToken cancel)
             where T : AsyncOperation
         {
             if (reportProgress != null)
